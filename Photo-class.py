11d0c341fc4c26f14e21324da0521c1786d26219
dmitry-stadnik <dmitry.stadnik.mail@gmail.com>
2015-05-19 19:48:19 +0500

    Updated Photo-class.py - added class definition (not finished)

--- Photo-class.py
+++ Photo-class.py
@@ -3,3 +3,10 @@
 # An uid(not name), name, path, datetime.
 # uid will be randomly generated, name, path will be original.
 # datetime will be extracted from EXIF or from MODIFIED date and time data.
+
+class Photo:
+    def __init__(self, directory, name):
+        self.directory = directory
+        self.name = name
+        self.datetime = None            # an instance of Photo will get datetime and uid
+        self.uid = None                 # later. So they're 'None' for now.
