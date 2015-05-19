5220f9c62e11ac7d14e2d9c896d90eec4921a3c7
dmitry-stadnik <dmitry.stadnik.mail@gmail.com>
2015-05-19 19:40:29 +0500

    Updated Photo-class.py - added description

--- Photo-class.py
+++ Photo-class.py
@@ -1,1 +1,5 @@
-#creating-Photo-class
+# The objective of that file is to create a Photo class.
+# An instance of the Photo should have the properties listed below:
+# An uid(not name), name, path, datetime.
+# uid will be randomly generated, name, path will be original.
+# datetime will be extracted from EXIF or from MODIFIED date and time data.
