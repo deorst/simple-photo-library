ff061cc3d73698aec1f73e825ab5322552b47fc2
dmitry-stadnik <dmitry.stadnik.mail@gmail.com>
2015-05-20 23:48:49 +0500

    started new branch 'creating-Library-class'

---
+++ Library-class.py
@@ -0,0 +1,5 @@
+# The objective of that file is to create a Library class.
+# An instance of the Library should have the properties listed below:
+# ..., current_uid (this value will assigned to the instances of the Photo class
+# one by one with Photo.get_uid() method), ...
+# 
