91e2fa39d948a2dd927fb6dbb2b5bc980136695f
dmitry-stadnik <dmitry.stadnik.mail@gmail.com>
2015-05-21 00:02:25 +0500

    added Library class definition

---
+++ branch_Library-class.py/Library-class.py
@@ -0,0 +1,19 @@
+# The objective of that file is to create a Library class.
+# An instance of the Library should have the properties listed below:
+# ..., current_uid (this value will assigned to the instances of the Photo class
+# one by one with Photo.get_uid() method), ...
+
+class Library:
+    def __init__(self, current_uid = 0):
+        self.current_uid = current_uid
+        
+    def __str__(self):
+        s = 'current uid:\t' + str(self.current_uid)
+        return s
+        
+# test phase
+
+library = Library(5)
+
+print library
+        
