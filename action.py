d144b0e2538520dc559eac1451127fc8f5d39e49
dmitry-stadnik <dmitry.stadnik.mail@gmail.com>
2015-05-30 14:21:55 +0500

    added action.py file where all is going on

---
+++ action.py
@@ -0,0 +1,18 @@
+from Photo import Photo
+from Library import Library
+from timeit import default_timer
+
+src_dir = raw_input('Please type in source directory: ')
+dst_dir = raw_input('Please type in destination directory: ')
+
+start = default_timer()                             # used for logging purposes
+library = Library(src_dir, dst_dir)
+
+library.read_all()
+library.make_new_dir()
+library.copy_src_to_dst()
+
+
+stop = default_timer()                              # used for logging purposes
+
+print '\rExecution time: ', stop - start, ' sec'
