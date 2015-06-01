f2d2f7173435638e7180af2b9b8059ea8a840c51
SuperManEver <nikita.luparev@gmail.com>
2015-06-01 12:11:55 +0500

    Add more comments

--- action.py
+++ action.py
@@ -5,19 +5,24 @@ Main script control the flow of the execution of whole program
 
 from Photo import Photo
 from Library import Library
+
+# for testing purposes
 from timeit import default_timer
 
+# get source and destination folder from user
 src_dir = raw_input('Please type in source directory: ')
 dst_dir = raw_input('Please type in destination directory: ')
 
-start = default_timer()                             # used for logging purposes
+# used for logging purposes
+start = default_timer()    
+
 library = Library(src_dir, dst_dir)
 
 library.read_all()
 library.make_new_dir()
 library.copy_src_to_dst()
 
-
-stop = default_timer()                              # used for logging purposes
+# used for logging purposes
+stop = default_timer()                              
 
 print '\rExecution time: ', stop - start, ' sec'
